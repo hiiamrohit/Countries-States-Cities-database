5435b7cfbf488b33d553c15a905faa9867367bcd
Javier Ortiz Bultron <javier.ortiz.78@gmail.com>
2018-04-05 08:10:31 -0500

    Add test to check for empty countries and cities.

--- src/test/java/com/github/hiiamrohit/ScriptTest.java
+++ src/test/java/com/github/hiiamrohit/ScriptTest.java
@@ -2,11 +2,14 @@ package com.github.hiiamrohit;
 
 import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
 import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -78,7 +81,59 @@ public class ScriptTest
   @Test
   public void testScripts() throws MalformedURLException
   {
-    //Now try to impor the data
+    //Now try to import the data
+    importData();
+  }
+
+  @Test
+  public void testEmptyCountries() throws MalformedURLException, SQLException
+  {
+    importData();
+    //Now query for empty Countries
+    PreparedStatement ps = conn.prepareStatement("select id,name from countries where id not in (select country_id from states)");
+    ResultSet rs = ps.executeQuery();
+    boolean isEmpty = !rs.isBeforeFirst() && rs.getRow() == 0;
+    int count = 0;
+    while (rs.next())
+    {
+      System.err.println(rs.getInt(1) + ") " + rs.getString(2));
+      count++;
+    }
+    if (count > 0)
+    {
+      System.err.println("There are " + count + " entries without children!");
+    }
+    assertTrue(isEmpty);
+  }
+
+  @Test
+  public void testEmptyStates() throws MalformedURLException, SQLException
+  {
+    importData();
+    //Now query for empty Countries
+    PreparedStatement ps = conn.prepareStatement("select s.id, s.name,(select name from countries where id=s.country_id) from states s where s.id not in (select state_id from cities)");
+    ResultSet rs = ps.executeQuery();
+    boolean isEmpty = !rs.isBeforeFirst() && rs.getRow() == 0;
+    int count = 0;
+    while (rs.next())
+    {
+      System.err.println(rs.getInt(1) + ") " + rs.getString(2) + " (" + rs.getString(3) + ")");
+      count++;
+    }
+    if (count > 0)
+    {
+      System.err.println("There are " + count + " entries without children!");
+    }
+    assertTrue(isEmpty);
+  }
+
+  /**
+   * Import scripts. It'll throw a RuntimeException if something goes wrong.
+   *
+   * @throws MalformedURLException
+   */
+  private void importData() throws MalformedURLException
+  {
     List<SqlScriptSource> scripts = new ArrayList<>();
     scripts.add(Sources.fromURL(new File("countries.sql").toURI().toURL()));
     scripts.add(Sources.fromURL(new File("states.sql").toURI().toURL()));
