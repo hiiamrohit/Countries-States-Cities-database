fe1780f37281bfa6feb961e4a95d44c76831fe07
DanOps! <odehdaniel1@gmail.com>
2022-05-18 11:23:03 +0100

    Updating sortname column

--- countries.sql
+++ countries.sql
@@ -8,7 +8,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `countries` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sortname` varchar(3) NOT NULL,
+  `shortname` varchar(3) NOT NULL,
   `name` varchar(150) NOT NULL,
   `phonecode` int(11) NOT NULL,
   PRIMARY KEY (`id`)
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `countries` (
 
 
 
-INSERT INTO `countries` (`id`, `sortname`, `name`, `phonecode`) VALUES
+INSERT INTO `countries` (`id`, `shortname`, `name`, `phonecode`) VALUES
 (1, 'AF', 'Afghanistan', 93),
 (2, 'AL', 'Albania', 355),
 (3, 'DZ', 'Algeria', 213),
